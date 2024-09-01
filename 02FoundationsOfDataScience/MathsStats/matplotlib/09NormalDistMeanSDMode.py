8d71d51b23db863583382aaf1331b086c3510e95
Anodiam <anirban@anodiam.com>
2024-09-01 19:53:01 +1000

    cmt

--- 02FoundationsOfDataScience/MathsStats/matplotlib/09NormalDistMeanSDMode.py
+++ 02FoundationsOfDataScience/MathsStats/matplotlib/09NormalDistMeanSDMode.py
@@ -14,7 +14,25 @@ print(f'scipy.stats.norm.ppf(0.68) = {q_68}')   # R equivalent of:: qnorm(0.68) 
 p_3_3 = sp.norm.cdf(3) - sp.norm.cdf(-3)   # R equivalent of:: pnorm(3) - pnorm(-3) = 0.9973002039367398
 print(f'scipy.stats.norm.cdf(3) - sp.norm.cdf(-3) = {p_3_3}')
 
-p_1sd = sp.norm.cdf(1) - sp.norm.cdf(-1)
+mu1, sigma1 = 10, np.sqrt(9)
+p_non_standard_12_10_9 = sp.norm.cdf(12, loc=mu1, scale=sigma1)    # R equivalent of:: pnorm(12, mean=10, sd=3)
+print(f'scipy.stats.norm.cdf(12, loc=mu, scale=sigma) = {p_non_standard_12_10_9}')
+
+print(f'********* 1.7: Ex3 *********')
+print(f'1. pnorm(-1, -2, sqrt(9)) = {sp.norm.cdf(-1, -2, np.sqrt(9))}')
+print(f'2. 1 - pnorm(-3.5, -2, sqrt(9)) = {1 - sp.norm.cdf(-3.5, -2, np.sqrt(9))}')
+print(f'3. pnorm(6, -2, sqrt(9) - pnorm(-7, -2, sqrt(9)) = '
+      f'{sp.norm.cdf(6, -2, np.sqrt(9)) - sp.norm.cdf(-7, -2, np.sqrt(9))}')
+print(f'****************************')
+
+full_range = 95/100
+bottom_range = (1-full_range)/2
+top_range = 1-bottom_range
+print(f'bottom_range = {bottom_range}, top_range = {top_range}, range = {top_range - bottom_range}')
+mu2, sigma2 = 174, 6.4
+q_bottom = sp.norm.ppf(bottom_range, loc=mu2, scale=sigma2)
+q_top = sp.norm.ppf(top_range, loc=mu2, scale=sigma2)
+print(f'{full_range * 100}% of heights is within {q_bottom} cm to {q_top} cm')
 
 bins = np.arange(mu-4*sigma, mu+4*sigma, .5)
 plt.hist(heights, bins=bins, edgecolor='black', color='pink', linewidth=1)
